3ca5a3fb7fdad6c2a08906dcc10f2a39c54ec5e6
joaoghg <jherreiragarnica@gmail.com>
2026-03-31 20:17:28 -0300

    refactor: simplify folder structure removing unecessary modules folder

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from '../config/env.validation';
+import { validate } from './config/env.validation';
 import { UserModule } from './user/users.module';
 import { AuthModule } from './auth/auth.module';
 
